4dd90ef5d2e86bf9e58b5f9f3cc4bb37182b204b
lanboys <lan_bing2013@163.com>
2018-09-19 18:48:21 +0800

    many2one关系

---
+++ src/main/java/com/bing/lan/hibernate/day02/_03_many2one/Employee.java
@@ -0,0 +1,40 @@
+package com.bing.lan.hibernate.day02._03_many2one;
+
+public class Employee {
+
+    private Long id;
+    private String name;
+    private Department department;
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
